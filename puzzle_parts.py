d3088b724057a7ffd1260f37917ba06aaa595a99
Alex Wang <iamalex11303@gmail.com>
2023-10-01 16:25:42 -0400

    implemented rotations

--- puzzle_parts.py
+++ puzzle_parts.py
@@ -2,16 +2,22 @@ class Cube:
     """
     A class to represent a 2x2 Rubik's cube.
     """
-    DEFAULT_STATE = [
-        [0, 0],
-        [0, 0],
+
+    DEFAULT_STATE: list[list[int]] = [
+              [0, 0],
+              [0, 0],
         [1, 1, 2, 2, 3, 3, 4, 4],
         [1, 1, 2, 2, 3, 3, 4, 4],
-        [5, 5],
-        [5, 5]
+              [5, 5],
+              [5, 5]
     ]
 
-    DEFAULT_GOAL = DEFAULT_STATE.copy()
+    DEFAULT_GOAL: list[list[int]] = DEFAULT_STATE.copy()
+
+    """
+    A set of unique valid moves.
+    """
+    MOVE_SET: set[str] = {"f", "u", "r", "f'", "u'", "r'"}
 
     def __init__(self) -> None:
         """
@@ -25,7 +31,8 @@ class Cube:
             5 5
             5 5
         
-        Each number in the cube represents a distinct color.
+        Each number in the cube represents a distinct color. The 2x2 face of 2s
+        are the front of the cube. 
         """
 
         self.state: list[int] = self.DEFAULT_STATE
@@ -50,7 +57,22 @@ class Cube:
 
         move = move.strip().lower()
         match move:
-            # TODO Implement movement.
+            case "f":
+                # Clockwise front side.
+                # Rotate front face.
+                self.state[2][2], self.state[2][3], self.state[3][2], self.state[3][3] = self.state[2][3], self.state[3][3], self.state[2][2], self.state[3][2]
+                # Rotate sides.
+                self.state[2][1], self.state[1][0], self.state[1][1], self.state[]
+            case "u":
+                # Clockwise upper side.
+            case "r":
+                # Clockwise right side.
+            case "f'":
+                # Counterclockwise front.
+            case "u'":
+                # Counterclockwise upper.
+            case "r'":
+                # Counterclockwise right.
             case _:
                 raise ValueError(f"The move {move} has not been implemented.")
 
